0eb00f41de4fc341d8ba75af790731d9cf55d6e0
Radek Raichl <radekraichl@gmail.com>
2021-12-07 22:29:35 +0100

    Přidejte soubory projektu.

---
+++ AnalyzaVstupu/Program.cs
@@ -0,0 +1,12 @@
+﻿ConsoleKeyInfo zadani;
+
+start:
+
+Console.WriteLine("Stiskni klávesu");
+zadani = Console.ReadKey(true);
+
+Console.WriteLine("Znak : " + zadani.KeyChar);
+Console.WriteLine("Klávesa : " + zadani.Key);
+Console.WriteLine("Modifikátory : " + (zadani.Modifiers == 0 ? "-" : zadani.Modifiers));
+
+goto start;
---
+++ ZadavaniVstupu/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ZadavaniVstupu
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ConsoleKey znak = Console.ReadKey(true).Key;
+            Console.WriteLine($"Stiskl jsi klavesu {znak}");
+        }
+    }
+}
