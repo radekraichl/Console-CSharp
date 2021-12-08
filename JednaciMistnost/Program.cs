ae83d4c610748e7c34925518ff0f594d04c81576
Radek Raichl <radekraichl@gmail.com>
2021-12-08 12:23:55 +0100

    Pridan projekt JednaciMistnost

---
+++ JednaciMistnost/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace JednaciMistnost
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("######  JEDNACÍ MÍSTNOST  ######");
+            Console.WriteLine("Při příchodu účastníka stiskni šipku nahoru, po odchodu stiskni šipku dolů.");
+            Console.WriteLine("Pro ukončení programu stiskni ESC.");
+            Console.WriteLine();
+
+            var jednaciMistnost = new JednaciMistnost(0);
+            Console.WriteLine(jednaciMistnost);
+
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    jednaciMistnost.PocetLidi++;
+                    Console.WriteLine(jednaciMistnost);
+                }
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    jednaciMistnost.PocetLidi--;
+                    Console.WriteLine(jednaciMistnost);
+                }
+            } 
+            while (keyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
