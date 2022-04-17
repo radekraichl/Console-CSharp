835d8382719ec062ecc475626ecc87233e3ee273
Radek Raichl <radekraichl@gmail.com>
2022-04-17 12:13:31 +0200

    Update InputAnalysis

---
+++ InputAnalysis/Program.cs
@@ -0,0 +1,13 @@
+﻿ConsoleKeyInfo input;
+
+do
+{
+    Console.WriteLine("Stiskni klávesu");
+    input = Console.ReadKey(true);
+
+    Console.WriteLine($"Znak : {input.KeyChar}");
+    Console.WriteLine($"Klávesa : {input.Key}");
+    Console.WriteLine($"Modifikátory :  {(input.Modifiers == 0 ? "-" : input.Modifiers)}");
+    Console.WriteLine();
+}
+while (input.Key != ConsoleKey.Escape);
