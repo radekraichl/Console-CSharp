8f3304660befe2620f28ee033ff7a0ca90466b7a
Radek Raichl <radekraichl@gmail.com>
2022-04-25 21:06:09 +0200

    Add project Colors

---
+++ Colors/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.Clear();
+
+Console.WriteLine("Následující věta bude tmavě zelená na žlutém pozadí.");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Tato věta je opravdu tmavě zelená na žlutém pozadí.");
+Console.WriteLine();
+
+ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+
+for (int colorNumber = 0; colorNumber < colors.Length; colorNumber++)
+{
+    Console.BackgroundColor = colorNumber == 0 ? ConsoleColor.Gray : ConsoleColor.Black;
+
+    Console.ForegroundColor = colors[colorNumber];
+    Console.WriteLine($"Barva číslo {colorNumber} je {colors[colorNumber]}");
+}
+
+Console.WriteLine();
+
+ConsoleColor[] colorsArray = { ConsoleColor.Red, ConsoleColor.Blue, ConsoleColor.Yellow };
+
+foreach (var color in colorsArray)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine($"Toto je barva {color} ({color:D})");
+}
+
+Console.ResetColor();
